25a358fe2b28f4f67bd4308f68ba947787895ac4
Chip-Mortensen <39780632+Chip-Mortensen@users.noreply.github.com>
2025-02-03 11:24:42 -0600

    Initial Commit

---
+++ Tiktok/TiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokApp.swift
+//  Tiktok
+//
+//  Created by Christian Mortensen on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
